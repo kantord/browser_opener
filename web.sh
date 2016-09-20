0a7943aabe3cc17dc690bd1c3d0ca57f0357c7dc
Dániel Kántor <git@daniel-kantor.com>
2016-09-20 10:49:45 +0200

    Fixed paths

--- web.sh
+++ web.sh
@@ -3,10 +3,11 @@
 if [ -z $@ ]
 then
     cd /home/kdani/repos/browser_opener
+    cd ~/repos/arch-config/browser_opener
     make places.txt
     cat "places.txt"
 else
-    cd /home/kdani/repos/browser_opener
+    cd ~/repos/arch-config/browser_opener
     URI=$@
     COMMAND=`echo $URI | cut -d'|' -f1`
     "./$COMMAND.sh" `echo $URI | cut -d'|' -f3` > /dev/null &
